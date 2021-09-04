4adfb39366fa15b4327b63fe75a8f32a8557ec20
Alex Thanas <athanas1@my.wctc.edu>
2021-09-03 20:33:30 -0500

    Initial commit

---
+++ src/main/java/edu/wctc/nwc/NwcApplication.java
@@ -0,0 +1,13 @@
+package edu.wctc.nwc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NwcApplication.class, args);
+    }
+
+}
